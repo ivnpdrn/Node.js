036acdbb49c7ee1ccc69af500e0cb09cfd0c8c1e
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-07-25 18:11:56 -0400

    use readFile() method toRead the file-non-blocking

--- test2.js
+++ test2.js
@@ -1,6 +1,16 @@
 var fs = require("fs");
 
-var data = fs.readFileSync("file1.txt", {encoding : "utf-8"});
+// var data = fs.readFileSync("file1.txt", {encoding : "utf-8"});
+
 console.log("File content:");
-console.log(data);
+
+fs.readFile("file1.txt", { encoding : "utf-8"},
+
+function(error, data) {
+
+    console.log(data);
+
+});
+
+console.log("The readFile () method was called")
 
