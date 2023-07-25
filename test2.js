0c806dad6a6f85659b9a67e02f9e0ea2a5cea26c
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-07-25 17:58:09 -0400

    display file contents as strings-file1.txt - utf-8

--- test2.js
+++ test2.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 
-var data = fs.readFileSync("file1.txt");
+var data = fs.readFileSync("file1.txt", {encoding : "utf-8"});
 console.log("File content:");
 console.log(data);
 
