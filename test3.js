3949a8e9fde63abfc8fb97f1b9a325b466303ab3
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-07-26 10:48:45 -0400

    using the random() method of the colors module

--- test3.js
+++ test3.js
@@ -1,3 +1,5 @@
 var colors = require("colors");
-console.log("colors = ", colors);
+console.log(colors.random("First text in random colors"));
+console.log(colors.random("Second text in random colors"));
+
 
